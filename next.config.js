c63dbb04c3de60483057665c62de515f73242d87
Oleksandr Poberezhniak <alexsmithshow@gmail.com>
2023-08-18 01:48:28 +0300

    Added .env.local file

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    env: {
+        STOREFRONT_API_KEY: process.env.STOREFRONT_API_KEY,
+        SHOPIFY_API_KEY: process.env.SHOPIFY_API_KEY,
+        SHOPIFY_API_SECRET_KEY: process.env.SHOPIFY_API_SECRET_KEY,
+        SHOPIFY_URL: process.env.SHOPIFY_URL
+    }
+}
 
 module.exports = nextConfig
